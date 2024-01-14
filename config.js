e63760d041507d6640c869fbd38e1c43ea4434a5
OsamuDazai67 <128489127+OsamuDazai67@users.noreply.github.com>
2024-01-14 11:53:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['918731025499', 'Shiyomi', true],
+  ['917005512394', 'Osamu Dazai', true], 
   [''] 
 ] //Number of owners
 
@@ -50,9 +50,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
+global.botname = 'NO LONGER HUMAN'
+global.premium = 'false'
+global.packname = '~𝐎𝐒𝐀𝐌𝐔 𝐃𝐀𝐙𝐀𝐈' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
